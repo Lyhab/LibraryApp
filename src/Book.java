77065cbbbe8cdabf90c6a7c550ecbc64839221c8
CheaTXT <serey.vichea9999@gmail.com>
2023-06-06 10:58:23 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,6 @@
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-
 public class Book {
     private final StringProperty bid;
     private final StringProperty title;
@@ -21,31 +20,84 @@ public class Book {
         borrow_date = new SimpleStringProperty(this, "borrow_date");
     }
 
-    public StringProperty bidProperty() { return bid;}
-    public String getBID() { return bid.get();}
-    public void setBID(String newBID) { bid.set(newBID);}
+    public StringProperty bidProperty() {
+        return bid;
+    }
 
-    public StringProperty titleProperty() { return title;}
-    public String getTitle() { return title.get();}
-    public void setTitle(String newTitle) { title.set(newTitle);}
+    public String getBID() {
+        return bid.get();
+    }
 
-    public StringProperty authorProperty() { return author;}
-    public String getAuthor() { return author.get();}
-    public void setAuthor(String newAuthor) { author.set(newAuthor);}
+    public void setBID(String newBID) {
+        bid.set(newBID);
+    }
 
-    public StringProperty languageProperty() { return language;}
-    public String getLanguage() { return language.get();}
-    public void setLanguage(String newLanguage) { bid.set(newLanguage);}
+    public StringProperty titleProperty() {
+        return title;
+    }
 
-    public StringProperty genreProperty() { return genre;}
-    public String getGenre() { return genre.get();}
-    public void setGenre(String newGenre) { genre.set(newGenre);}
+    public String getTitle() {
+        return title.get();
+    }
 
-    public StringProperty sidProperty() { return sid;}
-    public String getSID() { return sid.get();}
-    public void setSID(String newSID) { sid.set(newSID);}
+    public void setTitle(String newTitle) {
+        title.set(newTitle);
+    }
 
-    public StringProperty borrow_dateProperty() { return borrow_date;}
-    public String getBorrow_date() { return borrow_date.get();}
-    public void setBorrow_date(String newBorrow_date) { borrow_date.set(newBorrow_date);}
+    public StringProperty authorProperty() {
+        return author;
+    }
+
+    public String getAuthor() {
+        return author.get();
+    }
+
+    public void setAuthor(String newAuthor) {
+        author.set(newAuthor);
+    }
+
+    public StringProperty languageProperty() {
+        return language;
+    }
+
+    public String getLanguage() {
+        return language.get();
+    }
+
+    public void setLanguage(String newLanguage) {
+        bid.set(newLanguage);
+    }
+
+    public StringProperty genreProperty() {
+        return genre;
+    }
+
+    public String getGenre() {
+        return genre.get();
+    }
+
+    public void setGenre(String newGenre) {
+        genre.set(newGenre);
+    }
+
+    public StringProperty sidProperty() {
+        return sid;
+    }
+
+    public String getSID() {
+        return sid.get();
+    }
+
+    public void setSID(String newSID) {
+        sid.set(newSID);
+    }
+
+    // public StringProperty borrow_dateProperty() { return borrow_date;}
+    public String getBorrow_date() {
+        return borrow_date.get();
+    }
+
+    public void setBorrow_date(String newBorrow_date) {
+        borrow_date.set(newBorrow_date);
+    }
 }
