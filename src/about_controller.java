99e6b036062ce9a1062214335bf4cdf58456203e
CheaTXT <serey.vichea9999@gmail.com>
2023-06-19 09:41:50 +0700

    update

---
+++ src/about_controller.java
@@ -0,0 +1,34 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
+
+public class about_controller {
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
+
+  @FXML
+  void switchtoabout(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @FXML
+  void switchtoadmin(ActionEvent event) {
+
+  }
+
+  @FXML
+  void switchtohome(ActionEvent event) {
+
+  }
+
+}
--- src/home_controller.java
+++ src/home_controller.java
@@ -15,7 +15,11 @@ public class home_controller {
 
   @FXML
   void switchtoabout(ActionEvent event) throws IOException {
-
+    Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
   }
 
   @FXML
